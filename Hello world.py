41398d9bb89b8cfc74c50812ef704d1434a603f4
Frankmona <frankoguche1@.com>
2022-11-12 21:48:09 +0100

    defining function with conditional statement

--- Hello world.py
+++ Hello world.py
@@ -1,5 +1,10 @@
-def print_frank():
-    text = "Gooday! Thanks for contacting Frank"
-    print( text)
-    print( text)
-print_frank()
+def school_age_calculator(age, name):
+    if age < 5:
+        print("Enjoy your time you are still young")
+    elif age == 5:
+        print("Congratulations", name, "you are now eligible")
+    else:
+        print("Kids grow fast!")
+school_age_calculator( 4,"Frank")
+   
+
