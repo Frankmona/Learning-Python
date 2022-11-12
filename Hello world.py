e1cf182109bce18ae40c919caa8cf3104be4f40e
Frankmona <frankoguche1@.com>
2022-11-12 20:33:27 +0100

    defining Function

--- Hello world.py
+++ Hello world.py
@@ -1,9 +1,5 @@
-victor_age = 10
-High_school = 23
-
-if victor_age > High_school:
-    print("victor should be in High school")
-elif victor_age == High_school:
-    print("congratulations, you passed the age test!")
-else:
-    print("Dear victor, your are not eligible for this offer")
+def print_frank():
+    text = "Gooday! Thanks for contacting Frank"
+    print( text)
+    print( text)
+print_frank()
