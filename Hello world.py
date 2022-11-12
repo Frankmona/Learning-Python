86db002ab9ee5efe0e3491395ca89390010a7f2b
Frankmona <frankoguche1@.com>
2022-11-12 19:10:35 +0100

    example using conditional statements if, else and elif

--- Hello world.py
+++ Hello world.py
@@ -1,2 +1,9 @@
-victor_age = 32
-print(victor_age)
+victor_age = 10
+High_school = 23
+
+if victor_age > High_school:
+    print("victor should be in High school")
+elif victor_age == High_school:
+    print("congratulations, you passed the age test!")
+else:
+    print("Dear victor, your are not eligible for this offer")
