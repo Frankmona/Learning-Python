c0a174d2de4efa24479a61e990694107d878636d
Frankmona <frankoguche1@.com>
2022-11-14 17:13:59 +0100

    adding return to a function

--- Hello world.py
+++ Hello world.py
@@ -1,10 +1,8 @@
-def school_age_calculator(age, name):
-    if age < 5:
-        print("Enjoy your time you are still young")
-    elif age == 5:
-        print("Congratulations", name, "you are now eligible")
-    else:
-        print("Kids grow fast!")
-school_age_calculator( 4,"Frank")
+def add_ten_to_age(age):
+    new_age = 10 + age
+    return new_age
+How_old_would_he_be = add_ten_to_age(4)
+print(How_old_would_he_be)
+
    
 
